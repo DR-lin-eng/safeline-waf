877a1e4cba7903dec166399036f3ff86f3c3dfb5
DR-lin-eng <52230594+DR-lin-eng@users.noreply.github.com>
2025-04-14 21:22:05 +0800

    Update main.js

--- admin/frontend/src/main.js
+++ admin/frontend/src/main.js
@@ -1,49 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from 'axios'
-
-// 引入Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-
-// 引入Bootstrap JS
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
 // 配置axios
-axios.defaults.baseURL = process.env.VUE_APP_API_URL || '/api'
+axios.defaults.baseURL = process.env.VUE_APP_API_URL || '/safeline-admin-api'
 
 // 添加请求拦截器
 axios.interceptors.request.use(
   config => {
-    // 在请求发送前可以添加认证信息等
+    // 在请求发送前添加认证信息
+    const token = localStorage.getItem('auth_token') || sessionStorage.getItem('auth_token');
+    if (token) {
+      config.headers['Authorization'] = `Basic ${token}`;
+    }
     return config
   },
   error => {
     return Promise.reject(error)
   }
 )
-
-// 添加响应拦截器
-axios.interceptors.response.use(
-  response => {
-    return response
-  },
-  error => {
-    // 处理响应错误，例如身份验证错误
-    if (error.response && error.response.status === 401) {
-      router.push('/login')
-    }
-    return Promise.reject(error)
-  }
-)
-
-// 全局注册axios
-Vue.prototype.$http = axios
-
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
