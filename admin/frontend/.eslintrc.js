12dcab082d0fa4d2c73179c61e05a6eba7171577
DR-lin-eng <52230594+DR-lin-eng@users.noreply.github.com>
2025-04-14 21:04:55 +0800

    Update .eslintrc.js

--- admin/frontend/.eslintrc.js
+++ admin/frontend/.eslintrc.js
@@ -1,20 +1,11 @@
-module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
+rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // 放宽一些规则，以便于开发
     'vue/no-unused-components': 'warn',
-    'no-unused-vars': 'warn'
+    'no-unused-vars': 'warn',
+    // 禁用多词组件名称规则
+    'vue/multi-word-component-names': 'off',
+    // 禁用hasOwnProperty直接调用规则
+    'no-prototype-builtins': 'off'
   }
-}
